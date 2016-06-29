1c97147501f082427773db09209471e96abac0e3
Rob <rdaly1490@gmail.com>
2016-06-28 22:40:19 -0500

    got same basic line of sight stuff going

--- src/js/gameWithTiled.js
+++ src/js/gameWithTiled.js
@@ -18,6 +18,7 @@ var map;
 var groundLayer;
 var obstacleLayer;
 var player;
+var enemies;
 
 function create() {    
 
@@ -42,7 +43,6 @@ function create() {
 	groundLayer.resizeWorld();
 	obstacleLayer.resizeWorld();
 
-
 	//Add player
 	player = game.add.sprite(100, 100, 'player');
 	game.physics.arcade.enable(player);
@@ -50,6 +50,28 @@ function create() {
 
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+
+    ///////////// Test Enemies //////////////////
+    enemies = game.add.group();
+
+    var e1 = enemies.create(200, 120, 'player');
+    e1.isDefeated = false;
+    e1.frame = 5;
+	game.physics.arcade.enable(e1);
+    e1.body.immovable = true;
+
+    var e2 = enemies.create(200, 200, 'player');
+    e2.isDefeated = false;
+	game.physics.arcade.enable(e2);
+    e2.body.immovable = true;
+
+    var e3 = enemies.create(200, 250, 'player');
+    e3.isDefeated = true;
+	game.physics.arcade.enable(e3);
+    e3.body.immovable = true;
+    /////////////////////////////////////////////
+
 
     // Add camera
     game.camera.follow(player);
@@ -61,10 +83,8 @@ function create() {
 function update() {
 
 	// collide player with my layers from Tiled
-	// do I need to collide with ground since its basically just a background and no gravity here?
-	// game.physics.arcade.collide(player, groundLayer);
 	game.physics.arcade.collide(player, obstacleLayer);
-
+	game.physics.arcade.collide(player, enemies);
 
 	player.body.velocity.x = 0;
     player.body.velocity.y = 0;
@@ -95,7 +115,33 @@ function update() {
         player.frame = 4;
 
     }
-} 
+
+    drawLineOfSight();
+}
+
+function drawLineOfSight() {
+	enemies.forEach(function(enemy) {
+		var leftMax, leftMin, rightMax, rightMin
+
+		if (enemy.frame === 0) { // facing left
+			leftMax = enemy.x;
+			leftMin = enemy.x - 100;
+		} else if (enemy.frame === 5) { // facing right
+			rightMin = enemy.x;
+			rightMax = enemy.x + 100;			
+		}
+
+		if (player.x < leftMax && player.x > leftMin &&
+			player.y === enemy.y && enemy.isDefeated === false) {
+			console.log('Left Enemy');
+		}
+
+		if (player.x < rightMax && player.x > rightMin &&
+			player.y === enemy.y && enemy.isDefeated === false) {
+			console.log('right Enemy');
+		}
+	});
+}
 
 
 
